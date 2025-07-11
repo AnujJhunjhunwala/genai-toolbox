ced50b7f6857e7368f8cbda29324c2e49d5f565a
Anuj Jhunjhunwala <anujjjw@google.com>
2025-07-11 13:45:36 +0000

    Add dataplex-lookup-entry tool

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -1235,7 +1235,7 @@ func TestPrebuiltTools(t *testing.T) {
 			wantToolset: server.ToolsetConfigs{
 				"dataplex-tools": tools.ToolsetConfig{
 					Name:      "dataplex-tools",
-					ToolNames: []string{"search_entries"},
+					ToolNames: []string{"search_entries", "lookup_entry"},
 				},
 			},
 		},
